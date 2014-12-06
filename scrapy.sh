d1d2fb6036ea083e4c1f8a485ebc034e7649dfa7
linhaobuaa <1257819385@qq.com>
2014-12-06 10:13:47 +0800

    linhao little fix

---
+++ scrapy.sh
@@ -0,0 +1,27 @@
+scrapy crawl baidu_ns_search -a keywords_file='keywords_corp_baidu.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl weibo_api_search_spider -a keywords_file='keywords_corp_weiboapi.txt' --loglevel=INFO
+scrapy crawl xinhua_bbs_search -a keywords_file='keywords_corp_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl tianya_bbs_search -a keywords_file='keywords_corp_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl sogou_weixin_search -a keywords_file='keywords_corp_weixin.txt' -a start_datetime='2014-11-01 00:00:00' -a end_datetime='2014-12-01 00:00:00' --loglevel=INFO
+scrapy crawl baidu_ns_search -a keywords_file='keywords_leader_baidu.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl weibo_api_search_spider -a keywords_file='keywords_leader_weiboapi.txt' --loglevel=INFO
+scrapy crawl xinhua_bbs_search -a keywords_file='keywords_leader_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl tianya_bbs_search -a keywords_file='keywords_leader_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl sogou_weixin_search -a keywords_file='keywords_leader_weixin.txt' -a start_datetime='2014-11-01 00:00:00' -a end_datetime='2014-12-01 00:00:00' --loglevel=INFO
+scrapy crawl baidu_ns_search -a keywords_file='keywords_hot_baidu.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl weibo_api_search_spider -a keywords_file='keywords_hot_weiboapi.txt' --loglevel=INFO
+scrapy crawl xinhua_bbs_search -a keywords_file='keywords_hot_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl tianya_bbs_search -a keywords_file='keywords_hot_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --logleve    l=INFO
+scrapy crawl sogou_weixin_search -a keywords_file='keywords_leader_weixin.txt' -a start_datetime='2014-11-01 00:00:00' -a end_datetime='2014-12-01 00:00:00' --loglevel=INFO
+scrapy crawl baidu_ns_search -a keywords_file='keywords_enemy_baidu.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl weibo_api_search_spider -a keywords_file='keywords_enemy_weiboapi.txt' --loglevel=INFO
+scrapy crawl xinhua_bbs_search -a keywords_file='keywords_enemy_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl tianya_bbs_search -a keywords_file='keywords_enemy_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --logleve        l=INFO
+scrapy crawl sogou_weixin_search -a keywords_file='keywords_enemy_weixin.txt' -a start_datetime='2014-11-01 00:00:00' -a end_datetime='2014-12-01 00:00:00' --loglevel=INFO
+scrapy crawl baidu_ns_search -a keywords_file='keywords_friends_baidu.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl weibo_api_search_spider -a keywords_file='keywords _friends_weiboapi.txt' --loglevel=INFO
+scrapy crawl xinhua_bbs_search -a keywords_file='keywords_friends_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl tianya_bbs_search -a keywords_file='keywords_friends_forum.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl sogou_weixin_search -a keywords_file='keywords_friends_weixin.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl baidu_ns_search -a keywords_file='keywords_domain_baidu.txt' -a start_datetime="2014-11-01 00:00:00" -a end_datetime="2014-12-01 00:00:00" --loglevel=INFO
+scrapy crawl weibo_api_search_spider -a keywords_file='keywords_domain_weiboapi.txt' --loglevel=INFO
