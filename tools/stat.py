4784db984f2a4fa1f83a5203a359336ac93a3a8d
linhaobuaa <1257819385@qq.com>
2014-12-07 10:17:08 +0800

    linhao little fix

--- tools/stat.py
+++ tools/stat.py
@@ -50,7 +50,7 @@ def get_keywords(keywords_file):
     return keywords
 
 
-def sheqi_author_stat():
+def sheqi_stat():
     texts = []
     keywords = get_keywords('keywords_corp_baidu.txt')
 
@@ -187,6 +187,81 @@ def sheqi_author_stat():
     for keyword, count in keywords_results:
         fw.writerow((_encode_utf8(keyword), count))
 
+def enemy_stat():
+    texts = []
+    keywords = get_keywords('keywords_enemy_baidu.txt')
+
+    # 涉及企业的與情热度统计
+    corp_dict = dict()
+
+    # 统计关键词
+    total_keywords_list = []
+
+    query_dict["$or"] = [{"source_category": "keywords_enemy_weiboapi.txt"}]
+    query_dict["source_website"] = "weibo_api_search_spider"
+    count = mongo.master_timeline_weibo.find(query_dict).count()
+    results = mongo.master_timeline_weibo.find(query_dict)
+    for r in results:
+        texts.append([r['text'].encode('utf-8'), r['hot']])
+
+    query_dict["$or"] = [{"category": "keywords_enemy_forum.txt"}]
+    del query_dict["source_website"]
+    count = mongo.boatcol.find(query_dict).count()
+    results = mongo.boatcol.find(query_dict)
+    for r in results:
+        title = _encode_utf8(r['title'])
+        content168 = _encode_utf8(r['content168'])
+        summary = _encode_utf8(r['summary'])
+
+        text = title  + content168 + summary
+        texts.append([text, r['hot']))
+
+    query_dict["$or"] = [{"category": "keywords_enemy_weixin.txt"}]
+    count = mongo.boatcol.find(query_dict).count()
+    results = mongo.boatcol.find(query_dict)
+    for r in results:
+        title = _encode_utf8(r['title'])
+        content168 = _encode_utf8(r['content168'])
+        summary = _encode_utf8(r['summary'])
+
+        text = title  + content168 + summary
+        texts.append([text, r['hot']])
+
+    query_dict["$or"] = [{"category": "keywords_enemy_baidu.txt"}]
+    query_dict["source_website"] = "baidu_ns_search"
+    results = mongo.boatcol.find(query_dict)
+    for r in results:
+        title = _encode_utf8(r['title'])
+        content168 = _encode_utf8(r['content168'])
+        summary = _encode_utf8(r['summary'])
+
+        text = title  + content168 + summary
+        texts.append([text, r['hot']])
+
+    for text in texts:
+        cut_kw = cut(s, text[0])
+        total_keywords_list.extend(cut_kw)
+        for keyword in keywords:
+            if keyword in text[0]:
+                try:
+                    corp_dict[keyword] += text[1]
+                except KeyError:
+                    corp_dict[keyword] = text[1]
+
+    fw = csv.writer(open('enemy_gongsi_stat_%s_%s.csv' % (START_DATETIME, END_DATETIME), 'wb'), delimiter='^')
+    results = sorted(corp_dict.iteritems(), key=lambda(k, v): v, reverse=True)
+    for k, v in results:
+        if k == "":
+            continue
+        fw.writerow((_encode_utf8(k), v))
+
+    ct = collections.Counter(total_keywords_list)
+    keywords_results = ct.most_common(50)
+    fw = csv.writer(open('enemy_keywords_stat_%s_%s.csv' % (START_DATETIME, END_DATETIME), 'wb'), delimiter='^')
+    for keyword, count in keywords_results:
+        fw.writerow((_encode_utf8(keyword), count))
+
 
 if __name__=="__main__":
-    sheqi_author_stat()
+    #sheqi_stat()
+    enemy_stat()
