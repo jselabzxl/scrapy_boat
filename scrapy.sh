6a0db97eae23f269612b440784cc60efb68a63e2
linhaobuaa <1257819385@qq.com>
2014-12-07 15:21:08 +0800

    linhao little fix

--- scrapy.sh
+++ scrapy.sh
@@ -6,22 +6,22 @@ scrapy crawl baidu_ns_search -a keywords_file='keywords_corp_baidu.txt' -a start
 scrapy crawl weibo_api_search_spider -a keywords_file='keywords_corp_weiboapi.txt' --loglevel=INFO
 scrapy crawl xinhua_bbs_search -a keywords_file='keywords_corp_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl tianya_bbs_search -a keywords_file='keywords_corp_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
-scrapy crawl sogou_weixin_search -a keywords_file='keywords_corp_weixin.txt' -a start_datetime='2014-11-01 00:00:00' -a end_datetime='2014-12-01 00:00:00' --loglevel=INFO
+scrapy crawl sogou_weixin_search -a keywords_file='keywords_corp_weixin.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl baidu_ns_search -a keywords_file='keywords_leader_baidu.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl weibo_api_search_spider -a keywords_file='keywords_leader_weiboapi.txt' --loglevel=INFO
 scrapy crawl xinhua_bbs_search -a keywords_file='keywords_leader_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl tianya_bbs_search -a keywords_file='keywords_leader_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
-scrapy crawl sogou_weixin_search -a keywords_file='keywords_leader_weixin.txt' -a start_datetime='2014-11-01 00:00:00' -a end_datetime='2014-12-01 00:00:00' --loglevel=INFO
+scrapy crawl sogou_weixin_search -a keywords_file='keywords_leader_weixin.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl baidu_ns_search -a keywords_file='keywords_hot_baidu.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl weibo_api_search_spider -a keywords_file='keywords_hot_weiboapi.txt' --loglevel=INFO
 scrapy crawl xinhua_bbs_search -a keywords_file='keywords_hot_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl tianya_bbs_search -a keywords_file='keywords_hot_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --logleve    l=INFO
-scrapy crawl sogou_weixin_search -a keywords_file='keywords_leader_weixin.txt' -a start_datetime='2014-11-01 00:00:00' -a end_datetime='2014-12-01 00:00:00' --loglevel=INFO
+scrapy crawl sogou_weixin_search -a keywords_file='keywords_leader_weixin.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl baidu_ns_search -a keywords_file='keywords_enemy_baidu.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl weibo_api_search_spider -a keywords_file='keywords_enemy_weiboapi.txt' --loglevel=INFO
 scrapy crawl xinhua_bbs_search -a keywords_file='keywords_enemy_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
-scrapy crawl tianya_bbs_search -a keywords_file='keywords_enemy_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --logleve        l=INFO
-scrapy crawl sogou_weixin_search -a keywords_file='keywords_enemy_weixin.txt' -a start_datetime='2014-11-01 00:00:00' -a end_datetime='2014-12-01 00:00:00' --loglevel=INFO
+scrapy crawl tianya_bbs_search -a keywords_file='keywords_enemy_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
+scrapy crawl sogou_weixin_search -a keywords_file='keywords_enemy_weixin.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl baidu_ns_search -a keywords_file='keywords_friends_baidu.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
 scrapy crawl weibo_api_search_spider -a keywords_file='keywords _friends_weiboapi.txt' --loglevel=INFO
 scrapy crawl xinhua_bbs_search -a keywords_file='keywords_friends_forum.txt' -a start_datetime=$START_DATETIME_SCRAPY_BOAT -a end_datetime=$END_DATETIME_SCRAPY_BOAT --loglevel=INFO
